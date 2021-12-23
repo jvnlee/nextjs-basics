324f256a241eb8e4d6f561a01015d422305e64a7
jvnlee <lhjun1028@naver.com>
2021-12-23 23:50:24 +0900

    Learned basic routing mechanisms and component styling methods

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import NavBar from "../components/NavBar";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+      <style jsx global>{`
+        a {
+          color: gray;
+        }
+      `}</style>
+    </>
+  );
 }
 
-export default MyApp
+/*
+Global Settings
+
+"_App.js" is a file where you can create global scope settings, including global css, global layouts, etc.
+The "App" function takes 2 default props: "Component", "pageProps"
+
+- "Component" represents all the component files that is written by you.
+- "pageProps" represents the props of each component.
+
+"style" tags with "jsx", "global" props can set css of everything inside a component.
+When it is used in this "App" component, it will become the global scope css.
+*/
